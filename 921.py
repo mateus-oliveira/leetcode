ac6c61824a5a5afdc1af953081df6888aadd1128
Mateus Oliveira <devmateusalves@gmail.com>
2024-10-09 15:03:15 -0300

    refactor: 921 best runtime

--- 921.py
+++ 921.py
@@ -1,8 +1,8 @@
 """
 My solution to the 921 LeetCode's problem, a challenge from 9th October 2024.
 
-Runtime: 40 ms
-Allocated memory: 16.47 MB
+Runtime: 27 ms
+Allocated memory: 16.55 MB
 Complexity: O(n)
 
 https://leetcode.com/problems/minimum-add-to-make-parentheses-valid/description/
