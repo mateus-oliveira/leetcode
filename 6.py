dd566dd3e5bb6c489fcd1c4e938be0c1602bb6db
Mateus Oliveira <devmateusalves@gmail.com>
2024-10-13 13:38:05 -0300

    feat: problem 6

---
+++ 6.py
@@ -0,0 +1,25 @@
+"""
+My solution to the 6 LeetCode's problem.
+
+Runtime: 42 ms
+Allocated memory: 16.80 MB
+Complexity: O(n)
+
+https://leetcode.com/problems/zigzag-conversion/description/
+"""
+
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        zigzag_strings = ['']*numRows
+        
+        row = -1
+        up = False
+        for c in s:
+            row += -1 if up else 1
+            zigzag_strings[row] += c
+            if not up and row >= numRows-1:
+                up = True
+            elif up and row <= 0:
+                up = False
+
+        return ''.join(zigzag_strings)
