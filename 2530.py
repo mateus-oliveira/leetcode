ecc258eeb07758233249338144b386a48747fa6f
Mateus Oliveira <devmateusalves@gmail.com>
2024-10-20 16:51:17 -0300

    refactor: problem 2530

--- 2530.py
+++ 2530.py
@@ -6,19 +6,21 @@ class Solution:
         score = 0
 
         if len(nums) == 1:
-            for step in range(k):
+            for _ in range(k):
                 score += nums[0]
                 nums[0] = ceil(nums[0]/3)
             return score
 
         nums.sort()
         i = len(nums)-1
-        for step in range(k):
+        last = i
+        for _ in range(k):
             score += nums[i]
             nums[i] = ceil(nums[i]/3)
             i = 0 if i == 0 else i-1
-            if nums[i] < nums[-1]:
+            if nums[i] < nums[last]:
                 i = len(nums)-1
+                last = i
                 nums.sort()
 
         return score
