cd80d2418b62d1fc9520394ddfd01da3fccc8ed8
Mateus Alves de Oliveira <devmateusalves@gmail.com>
2024-10-25 11:12:25 -0300

    feat: problem 1233 in JS

---
+++ 1233.js
@@ -0,0 +1,26 @@
+/*
+My solution to the 1233 LeetCode's problem in Javascript.
+
+Runtime: 37 ms
+Allocated memory: 65.7 MB
+Complexity: O(n)
+
+https://leetcode.com/problems/remove-sub-folders-from-the-filesystem/description/
+*/
+
+/**
+ * @param {string[]} folder
+ * @return {string[]}
+ */
+var removeSubfolders = function(folder) {
+    folder.sort();
+    var folders = [folder[0]];
+    var last = 0;
+    for (f of folder) {
+        if (f == folders[last] || f.startsWith(`${folders[last]}/`))
+            continue;
+        folders.push(f);
+        last++;
+    }
+    return folders;
+};
